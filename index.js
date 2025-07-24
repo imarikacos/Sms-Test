8bc31ec4ec485bfa9651060272b348657b0bc2a7
imarikacos <imarikacos@gmail.com>
2025-07-24 16:17:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
+app.use(cors()); // Enable CORS
 app.use(express.json());
 
 // Send WhatsApp Message
